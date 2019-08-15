da030485c6c24ab0b178f7a61277eaed2c94a6b1
jhont285 <jhont285@gmail.com>
2019-08-15 08:54:30 -0500

    an advance in functional data structures

--- src/main/scala/fpinscala/datastructures/ListData.scala
+++ src/main/scala/fpinscala/datastructures/ListData.scala
@@ -1,5 +1,88 @@
 package fpinscala.datastructures
 
-object ListData {
-  def mod(n: Int, m: Int): Int = n % m
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object LinkedList {
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](as: List[A]): List[A] =
+    as match {
+      case Nil => Nil
+      case Cons(x, xs) => xs
+    }
+
+  def setHead[A](t: A, as: List[A]): List[A] =
+    as match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(t, xs)
+    }
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(x, xs) => if (n == 0) l else drop(xs, n - 1)
+    }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
+    }
+
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(x, xs) => if (xs == Nil) Nil else Cons(x, init(xs))
+    }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y)=> x + y)
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((x, y) => 1 + y)
+
+  @annotation.tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  def sum3(ns: List[Int]) = foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+
+  def reverse[A](ns: List[A], ap: List[A]): List[A] =
+    ns match {
+      case Nil => Nil
+      case Cons(x, Nil) => Cons(x, ap)
+      case Cons(x, xs) => reverse(xs, Cons(x, ap))
+    }
+
+  def reverse[A](ns: List[A]): List[A] = {
+    @annotation.tailrec
+    def go(as: List[A], current: List[A]): List[A] =
+      as match {
+        case Nil => current
+        case Cons(x, xs) => go(xs, Cons(x, current))
+      }
+
+    ns match {
+      case Nil => Nil
+      case Cons(x, xs) => go(xs, Cons(x, Nil))
+    }
+  }
+
+
 }
--- src/test/scala/fpinscala/datastructures/ListDataTest.scala
+++ src/test/scala/fpinscala/datastructures/ListDataTest.scala
@@ -3,7 +3,64 @@ package fpinscala.datastructures
 import org.scalatest.FunSuite
 
 class ListDataTest extends FunSuite {
-  test("CubeCalculator.cube") {
-    assert(ListData.mod(10, 5) == 0)
+  test("LinkedList.tail") {
+    assert(LinkedList.tail(LinkedList(1, 2, 3)).toString === "Cons(2,Cons(3,Nil))")
+    assert(LinkedList.tail(LinkedList(1)).toString === "Nil")
+    assert(LinkedList.tail(Nil).toString === "Nil")
   }
+
+  test("LinkedList.setHead") {
+    assert(LinkedList.setHead(5, LinkedList(1, 2, 3)).toString === "Cons(5,Cons(2,Cons(3,Nil)))")
+    assert(LinkedList.setHead(6, LinkedList(1)).toString === "Cons(6,Nil)")
+    assert(LinkedList.setHead(7, Nil).toString === "Nil")
+  }
+
+  test("LinkedList.drop") {
+    assert(LinkedList.drop(LinkedList(1, 2, 3), 1).toString === "Cons(2,Cons(3,Nil))")
+    assert(LinkedList.drop(LinkedList(1, 2, 3), 2).toString === "Cons(3,Nil)")
+    assert(LinkedList.drop(LinkedList(1, 2, 3), 3).toString === "Nil")
+    assert(LinkedList.drop(LinkedList(), 3).toString === "Nil")
+  }
+
+  test("LinkedList.dropWhile") {
+    assert(LinkedList.dropWhile(LinkedList(1, 2, 3), (x: Int) => x == -1).toString === "Cons(1,Cons(2,Cons(3,Nil)))")
+    assert(LinkedList.dropWhile(LinkedList(1, 2, 3), (x: Int) => x == 1).toString === "Cons(2,Cons(3,Nil))")
+    assert(LinkedList.dropWhile(LinkedList(1, 2, 3), (x: Int) => x < 3).toString === "Cons(3,Nil)")
+    assert(LinkedList.dropWhile(LinkedList(1, 2, 3), (x: Int) => x < 11).toString === "Nil")
+    assert(LinkedList.dropWhile(LinkedList(), (x: Int) => x != 1).toString === "Nil")
+  }
+
+  test("LinkedList.init") {
+    assert(LinkedList.init(LinkedList(1, 2, 3)).toString === "Cons(1,Cons(2,Nil))")
+    assert(LinkedList.init(LinkedList(1, 2)).toString === "Cons(1,Nil)")
+    assert(LinkedList.init(LinkedList(1)).toString === "Nil")
+    assert(LinkedList.init(LinkedList()).toString === "Nil")
+  }
+
+  test("LinkedList.length") {
+    assert(LinkedList.length(LinkedList(1, 2, 3)) === 3)
+    assert(LinkedList.length(LinkedList(1)) === 1)
+    assert(LinkedList.length(LinkedList()) === 0)
+  }
+
+  test("LinkedList.sum3") {
+    assert(LinkedList.sum3(LinkedList(1, 2, 3)) === 6)
+    assert(LinkedList.sum3(LinkedList(1)) === 1)
+    assert(LinkedList.sum3(LinkedList()) === 0)
+  }
+
+  test("LinkedList.product3") {
+    assert(LinkedList.product3(LinkedList(1, 2, 3)) === 6.0)
+    assert(LinkedList.product3(LinkedList(0, 2, 6)) === 0.0)
+  }
+
+  test("LinkedList.reverse") {
+    assert(LinkedList.reverse(LinkedList(1, 2, 3)).toString === "Cons(3,Cons(2,Cons(1,Nil)))")
+    assert(LinkedList.reverse(LinkedList(3, 2, 1)).toString === "Cons(1,Cons(2,Cons(3,Nil)))")
+    assert(LinkedList.reverse(LinkedList(2, 1)).toString === "Cons(1,Cons(2,Nil))")
+    assert(LinkedList.reverse(LinkedList(2)).toString === "Cons(2,Nil)")
+    assert(LinkedList.reverse(LinkedList()).toString === "Nil")
+  }
+
+
 }
