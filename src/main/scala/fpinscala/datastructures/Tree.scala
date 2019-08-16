220c55ab792e0c6fde076a6f94794f7fdf4cd693
jhont285 <jhont285@gmail.com>
2019-08-16 11:49:26 -0500

    an advance in functional data structures page 47

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,7 +1,5 @@
 package fpinscala.datastructures
 
-import scala.util.matching.Regex.Match
-
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
